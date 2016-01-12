3b6e751f4717e244debb50758111c79fdbbebae4
Pixel Member <pixelmember@Mikes-MacBook-Air-5.local>
2016-01-12 18:24:17 -0500

    Some progress on topics pages

--- topicstemplate.js
+++ topicstemplate.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
+	topics = {};
+	var topicCount = $(".topicTemplate").length;
 
+	for (var i = 0; i < topicCount; i++){
+		topics[$($(".topicTemplate")[i]).attr("id")] = i;
+	}
 
+ 	console.log(topics);
+	
+ 	prevTopic = function(topic){
+ 		var currentIndex = topics[topic];
+ 		$(".topicTemplate").each(function(){
+ 			$(this).css("display", "none");
+ 		});
+ 		var newIndex = (currentIndex-1)%topicCount;
+ 		console.log("#"+topics[newIndex])
+		$("#"+topics[newIndex]).css("display", "block");
+	}
+
+	nextTopic = function(){
+ 		$(".topicTemplate").each(function(){
+ 			$(this).css("display", "none");
+ 		});
+		$(topics[(id+1)%topics.length]).css("display", "block");
+	}
 });
 
