70b415192488b7a40669ef5d62ee97f5ed1e3c07
Pixel Staff <pixelstaff@Mikes-MacBook-Air-2.local>
2016-01-14 18:31:49 -0500

    Topics template  finished

--- topicstemplate.js
+++ topicstemplate.js
@@ -1,44 +1,52 @@
 $(document).ready(function(){
 	topics = [];
 
-	var topicCount = $(".topicTemplate").length;
+	var topicCount = $(".template").length;
 	for (var i = 0; i < topicCount; i++){
-		topics[i] = $($(".topicTemplate")[i]).attr("id");
+		topics[i] = $($(".template")[i]).attr("id");
 	}
+	console.log(topics);
 
 	var hash = window.location.hash;
-	console.log(hash);
 	if (window.location.hash !== ""){
 		var topic = hash.substr(1);
 		var currentIndex = topics.indexOf(topic);
  		if (currentIndex !== -1){
-	 		$(".topicTemplate").each(function(){
+	 		$(".template").each(function(){
 	 			$(this).css("display", "none");
 	 		});
 			$("#"+topics[currentIndex]).css("display", "block");
 		}
 	}
+	else{
+		$(".template").each(function(){
+	 			$(this).css("display", "none");
+ 		});
+		$("#"+topics[0]).css("display", "block");
+	}
 	
  	prevTopic = function(topic){
  		var currentIndex = topics.indexOf(topic);
  		if (currentIndex !== -1){
-	 		$(".topicTemplate").each(function(){
+	 		$(".template").each(function(){
 	 			$(this).css("display", "none");
 	 		});
 	 		var newIndex = ((currentIndex+topicCount)-1)%(topicCount);
 			$("#"+topics[newIndex]).css("display", "block");
 		}
+		console.log("success");
 	}
 
 	nextTopic = function(topic){
  		var currentIndex = topics.indexOf(topic);
  		if (currentIndex !== -1){
-	 		$(".topicTemplate").each(function(){
+	 		$(".template").each(function(){
 	 			$(this).css("display", "none");
 	 		});
 	 		var newIndex = (currentIndex+1)%(topicCount);
 			$("#"+topics[newIndex]).css("display", "block");
 		}
+		console.log("success");
 	}
 });
 
